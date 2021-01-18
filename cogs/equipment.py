d3f9ec58fd4486a15da2e552a288aafd35830c83
mcurranseijo <mcurranseijo@users.noreply.github.com>
2021-01-18 17:27:06 -0600

    Updates

--- cogs/equipment.py
+++ cogs/equipment.py
@@ -10,101 +10,193 @@ from difflib import get_close_matches
 BASE_URL = 'https://www.dnd5eapi.co'
 
 
-class equipment(commands.Cog):
+class Equipment(commands.Cog):
 
-  def __init__(self, bot):
-      self.bot = bot
-      self.session = aiohttp.ClientSession()
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = aiohttp.ClientSession()
+        with open("items.json") as file:
+            self.items = json.load(file)
 
-  @commands.command(aliases=['armor','weapon','e'])
-  async def equipment(self,ctx,*,args = None,level=None,page = 0,message = None,embed_data = None):
+    @commands.command(aliases=['e'])
+    async def equipment(self, ctx, *, args=None):
         try:
-            if not embed_data:
-                async with self.session.get(f'{BASE_URL}/api/equipment') as resp:
-                    data = json.loads(await resp.text())
-                count = data['count']
-                classes = data['results']
-                url_list = {}
-                for f in classes:
-                    url_list[f['name']] = f['url']
-                if args:
-                    values = get_close_matches(args, url_list)
-                    args = values[0]
-            
+            async with self.session.get(f'{BASE_URL}/api/equipment') as resp:
+                data = json.loads(await resp.text())
+            count = data['count']
+            classes = data['results']
+            url_list = {}
+            for f in classes:
+                url_list[f['name']] = f['url']
+            if args:
+                values = get_close_matches(args, url_list)
+                args = values[0]
+
             if not args:
-                embed = discord.Embed(title='D&D Equipment',description=f'{count} total items.',color=Color.red())
-                class_names = ""
-                for f in classes:
-                    if len(class_names)+len(f["name"]) > 1000:
-                        embed.add_field(name='__Item List:__',value=class_names)
-                        class_names = ""
-                    else:
-                        class_names+=f'\n*{f["name"]}*'
-                    
-                embed.add_field(name='__Item List:__',value=class_names)
-                    
+                embed = discord.Embed(title='D&D Equipment', description=f'{count} total items.', color=Color.red())
+                for category in self.items:
+                    per = int(len(self.items[category]['items'])/3)+1
+                    count = 0
+                    field = ""
+                    embed.add_field(name=category, value='_ _', inline=False)
+                    for item in self.items[category]['items']:
+                        count+=1
+                        print(f"{category}, {len(field)}")
+                        if count == per:
+                            count = 0
+                            embed.add_field(name="_ _", value=field)
+                            field = ""
+                        field+=f"{item['name']}\n"
+
+                    embed.add_field(name="_ _" , value=field)
+                    field = ""
+
                 await ctx.send(embed=embed)
                 return
-            
-            async with self.session.get(f'{BASE_URL}{url_list[args]}') as resp:
-                    data = json.loads(await resp.text())
-                    
-            
-            fields = []
-            if data['equipment_category']['name'] == "Armor":
-    	        fields.append(("Armor Category",data['armor_category'],True))
-    	        fields.append(("Armor Class",f"_Base:_ {data['armor_class']['base']}\n_Dex Bonus:_ {data['armor_class']['dex_bonus']}\n_Max Bonus:_ {data['armor_class']['max_bonus']}",True))
-    	        fields.append(("Strength Minimum",data['str_minimum'],True))
-    	        fields.append(("Stealth Disadvantage",data['stealth_disadvantage'],True))
-    	        fields.append(("Weight",data['weight'],True))
-    	        fields.append(("Cost",f"{data['cost']['quantity']} {data['cost']['unit']}",True))
-                
-            elif data['equipment_category']['name'] == "Weapon":
-                fields.append(("Weapon Category",data['weapon_category'],True))
-                fields.append(("Weapon Range",data['weapon_range'],True))
-                fields.append(("Cost",f"{data['cost']['quantity']} {data['cost']['unit']}",True))
-                fields.append(("Cost",f"{data['damage']['damage_dice']} {data['damage']['damage_type']['name']}",True))
-                fields.append(("Range",f"_Normal:_ {data['range']['normal']}\n_Long:_ {data['range']['long']}",True))
-                fields.append(("Weight",data['weight'],True))
-                properties = ""
-                
-                for f in data['properties']:
-                    properties+= f"{f['name']}, "
-                    
-                fields.append(("Properties",properties,False))
-    	            
-                
-            else:
-                fields.append(('Equipment Category',data['equipment_category']['name'],True))
-                
-                fields.append(('Gear Category',data['gear_category']['name'],True))
-                fields.append(('Cost',f"{data['cost']['quantity']} {data['cost']['unit']}",True))
-            
+
             async with self.session.get(f'{BASE_URL}{url_list[args]}') as resp:
                 data = json.loads(await resp.text())
-            
-            if data['equipment_category']['name'] != "Armor":
-                try:
-                    embed = discord.Embed(title=args,description=data['desc'][0],color=Color.red())
-                except KeyError:
-                    embed = discord.Embed(title=args,color=Color.red())
-            else:
-                try:
-                    embed = discord.Embed(title=args+' Armor',description=data['desc'][0],color=Color.red())
-                except KeyError:
-                    embed = discord.Embed(title=args+" Armor",color=Color.red())
-            
-            for name,value,inline in fields:
-                embed.add_field(name=name,value=value,inline=inline)
-             
-            if not message:
-                message = await ctx.send(embed=embed)
-            else:
-                await message.edit(embed=embed)
-        except TimeoutError:
-            return
+
+            fields = []
+            embed = discord.Embed(
+                title=data['name'],
+                color=Color.red()
+            )
+            embed.set_footer(text=data['equipment_category']['name'])
+            if data['equipment_category']['name'] == "Armor":
+                embed.title = f"{embed.title} Armor"
+                embed.description = f"{data['armor_category']} armor"
+                embed.add_field(
+                    name='Cost',
+                    value=f"{data['cost']['quantity']}{data['cost']['unit']}",
+                    inline=True
+                )
+                print(data['armor_class']['dex_bonus'])
+                if data['armor_class']['dex_bonus']:
+                    dex = "+ DEX"
+                else:
+                    dex = ""
+                embed.add_field(
+                    name='AC',
+                    value=f"{data['armor_class']['base']}{dex}",
+                    inline=True
+                )
+                if data['str_minimum']:
+                    embed.add_field(
+                        name='STR Min',
+                        value=data['str_minimum'],
+                        inline=True
+                    )
+                embed.add_field(name="_ _", value="_ _", inline=False)
+
+                embed.add_field(
+                    name='Stealth Disdavantage',
+                    value=data['stealth_disadvantage']
+                )
+
+                embed.add_field(
+                    name='Weight',
+                    value=data['weight']
+                )
+            elif data['equipment_category']['name'] == "Weapon":
+                embed.description = data['weapon_category']
+                embed.add_field(
+                    name='Cost',
+                    value=f"{data['cost']['quantity']}{data['cost']['unit']}",
+                    inline=True
+                )
+
+
+                embed.add_field(
+                    name='Damage',
+                    value=f"{data['damage']['damage_dice']} {data['damage']['damage_type']['name']}"
+                )
+                embed.add_field(name='_ _', value='_ _', inline=False)
+
+                embed.add_field(
+                    name='Weight',
+                    value=data['weight']
+                )
+
+                embed.add_field(
+                    name='Properties',
+                    value= '\n'.join([property['name'] for property in data['properties']])
+                )
+
+
+                if data['weapon_range'] == 'Ranged':
+                    embed.add_field(
+                        name='Range',
+                        value=f"Normal: {data['range']['normal']} {data['range']['long']}",
+                        inline=False
+                    )
+
+
+                embed.add_field(name="_ _", value="_ _", inline=False)
+            elif data['equipment_category']['name'] == "Tools":
+                if 'desc' in data:
+                    embed.description += f"\n{data['desc'][0]}"
+
+                embed.add_field(
+                    name='Cost',
+                    value=f"{data['cost']['quantity']}{data['cost']['unit']}",
+                    inline=True
+                )
+                embed.add_field(
+                    name='Weight',
+                    value=data['weight']
+                )
+            elif data['equipment_category']['name'] == "Adventuring Gear":
+                if 'desc' in data:
+                    embed.description = data['desc'][0]
+                elif 'contents' in data:
+                    embed.description = f"Includes: {', '.join([item['item']['name']+' x'+str(item['quantity']) for item in data['contents']])}"
+
+                embed.add_field(
+                    name='Cost',
+                    value=f"{data['cost']['quantity']}{data['cost']['unit']}",
+                    inline=True
+                )
+
+                if 'weight' in data:
+                    embed.add_field(
+                        name='Weight',
+                        value=data['weight']
+                    )
+            elif data['equipment_category']['name'] == "Mounts and Vehicles":
+
+                embed.description = data['vehicle_category']
+
+                if 'speed' in data:
+                    embed.add_field(
+                        name='Speed',
+                        value=f"{data['speed']['quantity']} {data['speed']['unit']}"
+                    )
+
+                if 'capacity' in data:
+                    embed.add_field(
+                        name='Capacity',
+                        value=data['capacity']
+                    )
+
+                embed.add_field(name='_ _', value='_ _', inline=True)
+
+                embed.add_field(
+                    name='Cost',
+                    value=f"{data['cost']['quantity']}{data['cost']['unit']}",
+                    inline=True
+                )
+
+                if 'weight' in data:
+                    embed.add_field(
+                        name='Weight',
+                        value=data['weight']
+                    )
+
+            await ctx.send(embed=embed)
         except IndexError:
-            await ctx.send(embed=discord.Embed(title=f'{args} not found',description='Please Try again.',color=Color.red()))
+            await ctx.send(
+                embed=discord.Embed(title=f'{args} not found', description='Please Try again.', color=Color.red()))
 
 def setup(bot):
-	bot.add_cog(equipment(bot))
+    bot.add_cog(Equipment(bot))
--- cogs/spells.py
+++ cogs/spells.py
@@ -11,14 +11,14 @@ BASE_URL = 'https://www.dnd5eapi.co'
 
 class spells(commands.Cog):
 
-  def __init__(self, bot):
-      self.bot = bot
-      self.session = aiohttp.ClientSession()
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = aiohttp.ClientSession()
 
-  @commands.command(aliases=['spell','s'])
-  async def spells(self,ctx,*,args = None,level=None,page = 0,message = None,embed_data = None):
-    try:
-        if not embed_data:
+    @commands.command(aliases=['spells', 's'])
+    async def spell(self, ctx, *, args=None):
+        try:
+
             async with self.session.get(f'{BASE_URL}/api/spells') as resp:
                 data = json.loads(await resp.text())
             count = data['count']
@@ -29,84 +29,83 @@ class spells(commands.Cog):
             if args:
                 values = get_close_matches(args, url_list)
                 args = values[0]
-        
-        if not args:
-            embed = discord.Embed(title='D&D Spells',description=f'{count} total spells.',color=Color.red())
-            class_names = ""
-            for f in classes:
-                if len(class_names)+len(f["name"]) > 1000:
-                    embed.add_field(name='__Spell List:__',value=class_names)
-                    class_names = ""
-                else:
-                    class_names+=f'\n*{f["name"]}*'
-                
-            embed.add_field(name='__Spell List:__',value=class_names)
-                
+
+            if not args:
+                embed = discord.Embed(title='D&D Spells', description=f'{count} total spells.', color=Color.red())
+
+
+                count = 0
+                field = ""
+                for spell in url_list:
+                    field += f"{spell}\n"
+                    if count == 54:
+                        embed.add_field(name='_ _', value=field)
+                        field = ""
+                        count = 0
+                    count += 1
+
+                embed.add_field(name='_ _', value=field)
+                await ctx.send(embed=embed)
+                return
+
+            async with self.session.get(f'{BASE_URL}{url_list[args]}') as resp:
+                data = json.loads(await resp.text())
+
+            embed = discord.Embed(
+                title=args,
+                description=f"\nLevel {data['level']} {data['school']['name']}",
+                color=Color.red())
+
+            embed.add_field(
+                name='Casting Time',
+                value=data['casting_time'],
+                inline=False
+            )
+
+            embed.add_field(
+                name='Range',
+                value=data['range'],
+                inline=False
+            )
+
+            if 'material' in data:
+                embed.add_field(
+                    name='Components',
+                    value=f"{', '.join(data['components'])}\n{data['material']}",
+                    inline=False
+                )
+            else:
+                embed.add_field(
+                    name='Components',
+                    value=f"{', '.join(data['components'])}",
+                    inline=False
+                )
+
+            embed.add_field(
+                name='Duration',
+                value=data['duration']
+            )
+
+            embed.add_field(
+                name='Description',
+                value=data['desc'][0],
+                inline=False
+            )
+
+            if 'higher_level' in data:
+                embed.add_field(
+                    name='Higher Level',
+                    value=data['higher_level'][0]
+                )
+
             await ctx.send(embed=embed)
-            return    
-        
-        async with self.session.get(f'{BASE_URL}{url_list[args]}') as resp:
-            data = json.loads(await resp.text())
-        
-        embed = discord.Embed(title=args,description=data['desc'][0],color=Color.red())
-        fields = []
-        try:
-            fields.append(("Higher Level",data['higher_level'][0],False))
-        except KeyError:
-            fields.append(("Higher Level",'N/A',False))
-        
-        try:
-            fields.append(("Range",data['range'],True))
-        except KeyError:
-            fields.append(("Range","N/A",True))
-            
-        try:
-            fields.append(("Components",', '.join(data['components']),True))
-        except KeyError:
-            fields.append(("Components","N/A",True))
-        
-        try:
-            fields.append(("Material",data['material'],True))
-        except KeyError:
-            fields.append(("Material","N/A",True))
-        
-        
-        try:
-            fields.append(("Ritual",data['ritual'],True))
-        except KeyError:
-            fields.append(("Ritual","N/A",True))
-            
-        try:
-            fields.append(("Concentration",data['concentratio'],True))
-        except KeyError:
-            fields.append(("Concentration","N/A",True))
-            
-        try:
-            fields.append(("Level",data['level'],True))
-        except KeyError:
-            fields.append(("Level",'N/A',True))
-            
-        try:
-            fields.append(("Attack Type",data['attack_type'],True))
-        except KeyError:
-            fields.append(("Attack Type",'N/A',True))
-            
-        try:
-            fields.append(("Damage Type",data['attack_type'],True))
-        except KeyError:
-            fields.append(("Attack Type",'N/A',True))
-        
-        for name,value,inline in fields:
-            embed.add_field(name=name,value=value,inline=inline)
-         
-        if not message:
-            message = await ctx.send(embed=embed)
-        else:
-            await message.edit(embed=embed)
-    except TimeoutError:
-        pass
-    except IndexError:
-        await ctx.send(embed=discord.Embed(title=f'{args} not found',description='Please Try again.',color=Color.red()))
+
+        except TimeoutError:
+            pass
+        except IndexError:
+            await ctx.send(
+                embed=discord.Embed(title=f'{args} not found', description='Please Try again.', color=Color.red()))
+
 
 def setup(bot):
-	bot.add_cog(spells(bot))
+    bot.add_cog(spells(bot))
